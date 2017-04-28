9fda2ae11cfd9bafa80f836c66e149f81e6aab56
Ricardo Lino Pereira Junior <ricardo.junior@cd.com>
2017-04-28 14:57:23 -0300

    renomeando modules

--- src/app/common/common.module.js
+++ src/app/common/common.module.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
-import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
-import User from './user/user';
+import Navbar from './navbar/navbar.module';
+import Hero from './hero/hero.module';
+import User from './user/user.module';
 
 let commonModule = angular.module('app.common', [
   Navbar,
